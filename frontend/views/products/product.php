33f72d00686299f1f5d1bab7ed8031a83e8cf133
LtFrager <sadesade2011@gmail.com>
2017-11-30 17:23:47 +0200

    tovar nedodelano

---
+++ frontend/views/products/product.php
@@ -0,0 +1,130 @@
+<div class="row">
+    <div class="col s12 m4 l3">
+        <?=\frontend\components\Category::widget()?>
+    </div>
+
+    <div class="col s12 m8 l9">
+
+        <div class="row">
+
+            <h2 class="title-h2" style="color:#000;">
+                Купить электровелосипед
+            </h2>
+
+            <p class="text-box">
+                Велосипед с двигателем – это новое слово в велосипедном производстве. Электро ВЕЛОСИПЕД – экологически чистый вид транспорта, т.к. используется электродвигатель. Для управления электровелосипедом не требуется ни ежегодное прохождение техосмотра, ни страховка, ни наличие прав, как для других видов транспорта. Велосипед с электромотором могут использовать люди всех возрастных категорий. С электровелосипедом подъемы на любые горки теперь не проблема, а если аккумулятор разрядится во время вашего пути, Вы доедете, просто крутя педали как на обычном велосипеде.
+                <br>
+                <span class="heavy-text-span">
+                 Электровелосипед - это один из самых доступных видов гибридного транспорта на сегодня.
+            </span>
+            </p>
+
+
+            <p class="text-box text-center" style="color:#000;">
+            <span class="heavy-text-span">
+                *ВСЕ ЦЕНЫ, УКАЗАННЫ КАК РЕКОМЕНДУЕМАЯ ДЛЯ РОЗНИЧНОЙ ПРОДАЖИ. В ИНТЕРНЕТ МАГАЗИНАХ, ЦЕНЫ НА ПРОДУКЦИЮ МОГУТ КОЛЕБАТЬСЯ В РАЗУМНЫХ ПРЕДЕЛАХ. МЫ ТАК ЖЕ ПРЕДОСТАВЛЯЕМ
+                <a href="#">СПИСОК ИНТЕРНЕТ МАГАЗИНОВ РАБОТАЮЩИХ С НАМИ</a>
+            </span>
+            </p>
+
+
+            <h2 class="title-h2" style="color:#373737;">
+                АККУМУЛЯТОРНАЯ БАТАРЕЯ ДЛЯ ЭЛЕКТРОВЕЛОСИПЕДОВ LIFEPO4 48V 10AH
+            </h2>
+
+
+            <div class="col s12 m6 l6" style="min-height: 300px">
+                <div class="galery">
+
+
+                    <div class="slider" id="slider">
+                        <div class="slider-bottom-shadow"></div>
+
+                        <ul class="slider__container">
+                            <li class="slider__item is-active">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-1_993_730.jpg);"></figure>
+                                <div class="slider__itemText">
+                                    <h2 class="slider__itemTitle">У ВЛАДЕЛЬЦА<br>
+                                        <span>электровелосипеда отсутствуют проблемы с пробками на дорогах</span></h2>
+                                </div>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-2_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-3_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-4_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-5_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-6_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-7_993_730.jpg)"></figure>
+                            </li>
+                            <li class="slider__item">
+                                <figure class="slider__itemImage" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-8_993_730.jpg)"></figure>
+                            </li>
+                        </ul>
+
+
+                        <div class="slider__prev">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="32" viewBox="-1 -1 12 32">
+                                <path d="M 10,0 0,15 10,30" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+                        <div class="slider__next is-active">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="32" viewBox="-1 -1 12 32">
+                                <path d="M0 0l10 15L0 30" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            </svg>
+                        </div>
+
+
+                        <ul class="navThumbs">
+                            <li class="navThumbs__item is_active" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-1_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-2_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-3_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-4_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-5_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-6_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-7_993_730.jpg)"></li>
+                            <li class="navThumbs__item" style="background-image:url(http://www.veola.com.ua/images/resized/images/stories/demo/slideshow/sl-8_993_730.jpg)"></li>
+                        </ul>
+                    </div>
+
+
+                </div>
+            </div>
+
+            <div class="">
+                <p class="price-index">
+                    <span style="color: black">Цена:</span> 1000 UH<br>
+                    <span style="font-size: 14px" class="old-price-index">Old price: <span class="crossed">500 UH</span></span>
+                </p>
+
+                <p style="float: left">Рейтинг: * * * * * </p>
+                <p style="float: right" class="">Хиты: (ХХХ)</p>
+                <br>
+                <br>
+                <p class="text-box">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa cupiditate doloribus, est expedita fugit hic, incidunt iusto nesciunt odio pariatur quam rerum vel? Alias aut autem dolores, rem suscipit voluptatem?
+                </p>
+            </div>
+
+        </div><!-- row-->
+
+    </div><!--col s8-->
+
+</div><!-- row-->
