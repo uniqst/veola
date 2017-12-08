bb5a81657cfd68248edf33ec552da2156753b279
xzACzx <zAC95zUA@gmail.com>
2017-12-08 16:08:13 +0200

    fixcommentcategory

--- backend/views/category/update.php
+++ backend/views/category/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Category */
 
-$this->title = 'Update Category: ' . $model->name;
+$this->title = 'Редактировать Категорию: ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Categories', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
--- backend/views/comments/view.php
+++ backend/views/comments/view.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'confirm' => Yii::t('app', 'Вы уверены, что хотите удалить этот элемент?'),
                 'method' => 'post',
             ],
         ]) ?>
