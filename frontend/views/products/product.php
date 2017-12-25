e15474ef7cfb6c97fe192eef31f0b65b8e727f30
xzACzx <zAC95zUA@gmail.com>
2017-12-25 13:45:34 +0200

    fixproducts

--- frontend/views/products/product.php
+++ frontend/views/products/product.php
@@ -58,17 +58,6 @@ if(Yii::$app->session['rates'] == 'grn' or empty(Yii::$app->session['rates'])){
 
                 <p class="price-index">
                     <span style="color: black">Цена:</span> <?=$model->price * $rates . ' ' . $ex?><br>
-                    <form method="POST" accept-charset="utf-8" action="https://www.liqpay.ua/api/3/checkout">
-	<input type="hidden" name="data" value="eyJ2ZXJzaW9uIjozLCJhY3Rpb24iOiJwYXkiLCJwdWJsaWNfa2V5IjoiaTQxNDU5MTM0MDg0IiwiYW1vdW50IjoiNSIsImN1cnJlbmN5IjoiVUFIIiwiZGVzY3JpcHRpb24iOiLQnNC+0Lkg0YLQvtCy0LDRgCIsInR5cGUiOiJidXkiLCJsYW5ndWFnZSI6InJ1In0=" />
-	<input type="hidden" name="signature" value="n6FlBybAH9yK+TLM/GZICaXv0cg=" />
-	<button style="border: none !important; display:inline-block !important;text-align: center !important;padding: 7px 20px !important;
-		color: #fff !important; font-size:16px !important; font-weight: 600 !important; font-family:OpenSans, sans-serif; cursor: pointer !important; border-radius: 2px !important;
-		background: rgb(122,183,43) !important;"onmouseover="this.style.opacity='0.5';" onmouseout="this.style.opacity='1';">
-		<img src="https://static.liqpay.ua/buttons/logo-small.png" name="btn_text"
-			style="margin-right: 7px !important; vertical-align: middle !important;"/>
-		<span style="vertical-align:middle; !important">Оплатить 5 UAH</span>
-	</button>
-</form>
                     <span style="font-size: 14px" class="old-price-index">Старая цена: <span class="crossed"><?=$model->old_price * $rates . ' ' . $ex?></span></span>
                 </p>
 
