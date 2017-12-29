8e7b0bbfe4035a140c27d889dd11a7043f3865c8
Galifax <galifax94@gmail.com>
2017-12-29 02:11:14 +0200

    sort first

--- frontend/views/products/category.php
+++ frontend/views/products/category.php
@@ -30,6 +30,13 @@
                 </span>
                 <select class="browser-default" id="sort" name="sort" size="1" onchange="this.form.submit()" style="width: 150px;">
                     <option value="name" <?php if($_GET['sort'] == 'name') echo 'selected' ?>>Название</option>
+                    
+                    <option value="-standart" <?php if($_GET['sort'] == '-standart') echo 'selected' ?>>Стандатрные</option>
+                    <option value="-promo" <?php if($_GET['sort'] == '-promo') echo 'selected' ?>>Акционные</option>
+                    <option value="-sale" <?php if($_GET['sort'] == '-sale') echo 'selected' ?>>Распродажа</option>
+                    <option value="-prepared" <?php if($_GET['sort'] == '-prepared') echo 'selected' ?>>Ожидаются</option>
+                    <option value="-new" <?php if($_GET['sort'] == '-new') echo 'selected' ?>>Новые</option>
+
                     <option value="-price" <?php if($_GET['sort'] == '-price') echo 'selected' ?>>Цена по убыванию</option>
                      <option value="price" <?php if($_GET['sort'] == 'price') echo 'selected' ?>>Цена по возврастанию</option>
                     <option value="date" <?php if($_GET['sort'] == 'date' or !$_GET['sort']) echo 'selected' ?>>Дата</option>
